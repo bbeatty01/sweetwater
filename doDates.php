d27c880bd059a52e36fcfd7bc1f41cd821d755d7
Brandon M Beatty <68614179+bbeatty01@users.noreply.github.com>
2021-05-10 20:03:10 -0400

    Add files via upload

--- doDates.php
+++ doDates.php
@@ -8,7 +8,7 @@
         $sql = "UPDATE sweetwater_test SET 'shipdate_expected' = '$newDate' WHERE orderid = '$newId'";
         $conn->query($sql);
     }
-    $sql = "SELECT REGEXP_SUBSTR(comments, '([0-9]{2}/[0-9]{2})/[0-9]{2}') AS dates, orderid FROM sweetwater_test WHERE REGEXP_SUBSTR(comments, '([0-9]{2}/[0-9]{2})/[0-9]{2}') != ''";
+    $sql = "SELECT REGEXP_SUBSTR(comments, '([0-9]{2}/[0-9]{2})/[0-9]{2}') AS dates FROM sweetwater_test WHERE REGEXP_SUBSTR(comments, '([0-9]{2}/[0-9]{2})/[0-9]{2}') != ''";
     $dates = $conn->query($sql);
     print_r($dates);
 ?>
