4f24e209736b9c3c896c60f7716bce2f79ef9406
Brandon M Beatty <68614179+bbeatty01@users.noreply.github.com>
2021-05-10 20:00:48 -0400

    Add files via upload

--- doDates.php
+++ doDates.php
@@ -3,10 +3,12 @@
     $sql = "SELECT REGEXP_SUBSTR(comments, '([0-9]{2}/[0-9]{2})/[0-9]{2}') AS dates, orderid FROM sweetwater_test WHERE REGEXP_SUBSTR(comments, '([0-9]{2}/[0-9]{2})/[0-9]{2}') != ''";
     $dates = $conn->query($sql);
     foreach ($dates as $date) {
-        print_r($date->dates);
-        //"UPDATE sweetwater_test SET 'shipdate_expected' = {$new} WHERE orderid = \"{$date['orderid']}\"";
+        $newDate = $date['dates'];
+        $newId = $date['orderid'];
+        $sql = "UPDATE sweetwater_test SET 'shipdate_expected' = '$newDate' WHERE orderid = '$newId'";
+        $conn->query($sql);
     }
     $sql = "SELECT REGEXP_SUBSTR(comments, '([0-9]{2}/[0-9]{2})/[0-9]{2}') AS dates, orderid FROM sweetwater_test WHERE REGEXP_SUBSTR(comments, '([0-9]{2}/[0-9]{2})/[0-9]{2}') != ''";
     $dates = $conn->query($sql);
-    //echo $dates["dates"];
+    print_r($dates);
 ?>
